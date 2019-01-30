f9d5b1d0e09d6a34641538b357eb6c6a2dc66c8b
Michael Walther <michael.walther@cea.fr>
2019-01-30 15:06:50 +0100

    changed things such that only if a resolution matrix is existing the boundary pixels are discarded for reso

--- py/picca/data.py
+++ py/picca/data.py
@@ -197,9 +197,14 @@ class forest(qso):
 
         # compute means
         if reso is not None :
-            self.mean_reso = sp.mean(reso[4:-4]) #* constants.speed_light * 1000. * forest.dll * sp.log(10.0) #as I gave it reso_pix instead of km/s
+            if reso_matrix:
+                nremove=reso_matrix.shape[0]//2
+                self.mean_reso = sp.mean(reso[nremove:-nremove]) #* constants.speed_light * 1000. * forest.dll * sp.log(10.0) #as I gave it reso_pix instead of km/s
+            else:
+                self.mean_reso = sp.mean(reso)
         if reso_matrix is not None:
-            self.mean_reso_matrix = sp.mean(reso_matrix[:,4:-4],axis=1)   #this might be extended by properly filtering out pixels where boundary effects play a role (instead of just removing 4 pixels on each side). This will also return an empty array for short spectra (and the FFT of this will be nan)
+            nremove=reso_matrix.shape[0]//2
+            self.mean_reso_matrix = sp.mean(reso_matrix[:,nremove:-nremove],axis=1)   #this might be extended by properly filtering out pixels where boundary effects play a role (instead of just removing 4 pixels on each side). This will also return an empty array for short spectra (and the FFT of this will be nan)
         err = 1.0/sp.sqrt(iv)
         SNR = fl/err
         self.mean_SNR = sp.mean(SNR)
